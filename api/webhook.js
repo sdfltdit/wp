02d23c739d11ab84d9872af7925f1670d06674da
sdfltdit <126612571+sdfltdit@users.noreply.github.com>
2026-06-16 16:59:04 +0600

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -348,7 +348,7 @@ module.exports = async function handler(req, res) {
     const token = urlObj.searchParams.get("hub.verify_token");
     const challenge = urlObj.searchParams.get("hub.challenge");
     if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
-      return res.status(200).send(challenge);
+      return res.status(200).send(challenge + '');
     }
     return res.status(403).send("Forbidden");
   }
